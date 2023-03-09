b5b391b2fc1ae72b9ea6502f65cd3af03e754b2c
Lenon <lenonphilippi@gmail.com>
2023-03-09 14:40:24 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return <div>Hello, TrybeWallet! I said: HELLO, TRYBEWALLET!</div>;
 }
 
 export default App;
